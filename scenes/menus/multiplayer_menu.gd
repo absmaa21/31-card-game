3a171318a3a957465b82d3ab7131bceea1192fa6
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-10-21 17:19:57 +0200

    update getting player_data logic

--- scenes/menus/multiplayer_menu.gd
+++ scenes/menus/multiplayer_menu.gd
@@ -23,11 +23,17 @@ func _on_lobby_match_list(these_lobbies: Array) -> void:
 	for child: Node in lobby_list.get_children():
 		child.queue_free()
 
+	var invalid_lobbies: int = 0
 	for this_lobby: int in these_lobbies:
-		var lobby_name: String = Steam.getLobbyData(this_lobby, "name")
-		var lobby_mode: String = Steam.getLobbyData(this_lobby, "mode")
+		var lobby_data: Dictionary = Steam.getAllLobbyData(this_lobby)
+		if not Steam.getAllLobbyData(this_lobby).has_all(["name", "mode", "max_players"]):
+			invalid_lobbies += 1
+			continue
+
+		var lobby_name: String = lobby_data.get("name")
+		var lobby_mode: String = lobby_data.get("mode")
 		var lobby_num_members: int = Steam.getNumLobbyMembers(this_lobby)
-		var lobby_max_players: int = int(Steam.getLobbyData(this_lobby, "max_players"))
+		var lobby_max_players: int = int(lobby_data.get("max_players"))
 
 		var button: LobbyButton = LOBBY_BUTTON.instantiate()
 		lobby_list.add_child(button)
@@ -36,4 +42,8 @@ func _on_lobby_match_list(these_lobbies: Array) -> void:
 		button.lobby_mode.text = lobby_mode
 		button.player_count.text = "%d / %d" % [lobby_num_members, lobby_max_players]
 
-	lobby_count.text = "%d lobbies found" % these_lobbies.size()
+	print("Out of %d lobbies, %d were invalid" % [these_lobbies.size(), invalid_lobbies])
+	if lobby_list.get_child_count() > 0:
+		lobby_count.text = "%d lobbies found" % lobby_list.get_child_count()
+	else:
+		lobby_count.text = "No lobbies found"
